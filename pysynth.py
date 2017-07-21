9b171e75837ebd9217196a468cb06faad97bf79a
zuds <georgemirano@gmail.com>
2017-07-21 00:18:38 -0400

    Add files via upload

--- pysynth.py
+++ pysynth.py
@@ -162,31 +162,17 @@ def make_wav(song,bpm=120,transpose=0,pause=.05,boost=1.1,repeat=0,fn="out.wav",
 	f.close()
 	print()
 
-##########################################################################
-# Synthesize demo songs
-##########################################################################
+#############################################################################
+# Here I use the make_wav function to convert my list of notes to a .wav file
+#############################################################################
 
 if __name__ == '__main__':
 	print()
 	print("Creating Demo Songs... (this might take about a minute)")
 	print()
 
-	# SONG 1
-	make_wav(song1, fn = "pysynth_scale.wav")
+	make_wav(MaskOff, bpm = 150, fn = "MaskOff.wav")
 
-	# SONG 2
-	make_wav(song2, bpm = 95, boost = 1.2, fn = "pysynth_anthem.wav")
-
-	# SONG 3
-	make_wav(song3, bpm = 132/2, pause = 0., boost = 1.1, fn = "pysynth_chopin.wav")
-
-	# SONG 4
-	#   right hand part
-	make_wav(song4_rh, bpm = 130, transpose = 1, pause = .1, boost = 1.15, repeat = 1, fn = "pysynth_bach_rh.wav")
-	#   left hand part
-	make_wav(song4_lh, bpm = 130, transpose = 1, pause = .1, boost = 1.15, repeat = 1, fn = "pysynth_bach_lh.wav")
-	#   mix both files together
-	mix_files("pysynth_bach_rh.wav", "pysynth_bach_lh.wav", "pysynth_bach.wav")
 
 
 
